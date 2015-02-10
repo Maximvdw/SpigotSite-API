bbfe31e661f70389cd9a91348ec283adc49b9a03
Maxim Van de Wynckel <maxim@mvdw-software.be>
2015-02-10 18:22:52 +0100

    bug syntax

--- src/main/java/be/maximvdw/spigotsite/api/exceptions/SpamWarningException.java
+++ src/main/java/be/maximvdw/spigotsite/api/exceptions/SpamWarningException.java
@@ -1,11 +1,6 @@
 package be.maximvdw.spigotsite.api.exceptions;
 
-import be.maximvdw.spigotsite.user.SpigotUser;
-
-public class 	SpigotUser spigotUser = (SpigotUser) user;
-for (String cookie : spigotUser.getCookies().keySet()) {
-	System.out.println(cookie);
-} extends RuntimeException {
+public class SpamWarningException extends RuntimeException {
 	private static final long serialVersionUID = 854651688L;
 
 }
