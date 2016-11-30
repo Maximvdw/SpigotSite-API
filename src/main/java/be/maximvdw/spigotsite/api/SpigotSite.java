d42c1c7827c73b618650f10aedfea9d585195d3f
Maxim Van de Wynckel <maxim@mvdw-software.com>
2016-11-30 12:38:55 +0100

    remove buyer

--- src/main/java/be/maximvdw/spigotsite/api/SpigotSite.java
+++ src/main/java/be/maximvdw/spigotsite/api/SpigotSite.java
@@ -4,7 +4,7 @@ public final class SpigotSite {
 	/* Spigot Site API */
 	private static SpigotSiteAPI api = null;
 	/* Spigot Site API Version */
-	private static String version = "0.0.4-SNAPSHOT";
+	private static String version = "0.0.6-SNAPSHOT";
 
 	/**
 	 * Get spigot site API
--- src/main/java/be/maximvdw/spigotsite/api/resource/ResourceManager.java
+++ src/main/java/be/maximvdw/spigotsite/api/resource/ResourceManager.java
@@ -7,188 +7,170 @@ import be.maximvdw.spigotsite.api.user.User;
 
 /**
  * Spigot Resource Manager
- * 
+ *
  * @author Maxim Van de Wynckel
  */
 public interface ResourceManager {
-	/**
-	 * Get a {@link be.maximvdw.spigotsite.api.resource.Resource} by identifier
-	 * 
-	 * @param resourceid
-	 *            Resource identifier
-	 * @return Spigot Resource
-	 */
-	public Resource getResourceById(int resourceid);
-	
-	/**
-	 * Get a {@link be.maximvdw.spigotsite.api.resource.Resource} by identifier
-	 * 
-	 * @param resourceid
-	 *            Resource identifier
-	 * @param user Authenticated spigot user
-	 * @return Spigot Resource
-	 */
-	public Resource getResourceById(int resourceid,User user);
+    /**
+     * Get a {@link be.maximvdw.spigotsite.api.resource.Resource} by identifier
+     *
+     * @param resourceid Resource identifier
+     * @return Spigot Resource
+     */
+    Resource getResourceById(int resourceid);
 
-	/**
-	 * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
-	 * {@link be.maximvdw.spigotsite.api.user.User}
-	 * 
-	 * @param user
-	 *            {@link be.maximvdw.spigotsite.api.user.User}
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	public List<Resource> getResourcesByUser(User user);
-	
-	/**
-	 * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
-	 * {@link be.maximvdw.spigotsite.api.user.User}
-	 * 
-	 * @param user
-	 *            {@link be.maximvdw.spigotsite.api.user.User}
-	 * @param loggedInUser {@link be.maximvdw.spigotsite.api.user.User} used to get hidden plugins
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	public List<Resource> getResourcesByUser(User user,User loggedInUser);
+    /**
+     * Get a {@link be.maximvdw.spigotsite.api.resource.Resource} by identifier
+     *
+     * @param resourceid Resource identifier
+     * @param user       Authenticated spigot user
+     * @return Spigot Resource
+     */
+    Resource getResourceById(int resourceid, User user);
 
-	/**
-	 * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
-	 * {@link be.maximvdw.spigotsite.api.user.User}
-	 * 
-	 * @param id
-	 *            User identifier
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	public List<Resource> getResourcesByUser(int id);
-	
-	/**
-	 * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
-	 * {@link be.maximvdw.spigotsite.api.user.User}
-	 * 
-	 * @param id
-	 *            User identifier
-	 * @param loggedInUser {@link be.maximvdw.spigotsite.api.user.User} used to get hidden plugins
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	public List<Resource> getResourcesByUser(int id,User loggedInUser);
+    /**
+     * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
+     * {@link be.maximvdw.spigotsite.api.user.User}
+     *
+     * @param user {@link be.maximvdw.spigotsite.api.user.User}
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getResourcesByUser(User user);
 
-	/**
-	 * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} bought
-	 * by a {@link be.maximvdw.spigotsite.api.user.User}
-	 * 
-	 * @param user
-	 *            Authenticated {@link be.maximvdw.spigotsite.api.user.User}
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	public List<Resource> getPurchasedResources(User user)
-			throws ConnectionFailedException;
+    /**
+     * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
+     * {@link be.maximvdw.spigotsite.api.user.User}
+     *
+     * @param user         {@link be.maximvdw.spigotsite.api.user.User}
+     * @param loggedInUser {@link be.maximvdw.spigotsite.api.user.User} used to get hidden plugins
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getResourcesByUser(User user, User loggedInUser);
 
-	/**
-	 * Get resource categories on spigot
-	 * 
-	 * @return List of
-	 *         {@link be.maximvdw.spigotsite.api.resource.ResourceCategory}
-	 */
-	public List<ResourceCategory> getResourceCategories();
+    /**
+     * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
+     * {@link be.maximvdw.spigotsite.api.user.User}
+     *
+     * @param id User identifier
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getResourcesByUser(int id);
 
-	/**
-	 * Get {@link be.maximvdw.spigotsite.api.resource.ResourceCategory} by
-	 * identifier
-	 * 
-	 * @param id
-	 *            Resource Category identifier
-	 * @return Resource Category
-	 */
-	public ResourceCategory getResourceCategoryById(int id);
+    /**
+     * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} from a
+     * {@link be.maximvdw.spigotsite.api.user.User}
+     *
+     * @param id           User identifier
+     * @param loggedInUser {@link be.maximvdw.spigotsite.api.user.User} used to get hidden plugins
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getResourcesByUser(int id, User loggedInUser);
 
-	/**
-	 * Get all resources from a specific category
-	 * 
-	 * @param category
-	 *            List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 * @return
-	 */
-	public List<Resource> getResourcesByCategory(ResourceCategory category);
+    /**
+     * Get a list of {@link be.maximvdw.spigotsite.api.resource.Resource} bought
+     * by a {@link be.maximvdw.spigotsite.api.user.User}
+     *
+     * @param user Authenticated {@link be.maximvdw.spigotsite.api.user.User}
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getPurchasedResources(User user)
+            throws ConnectionFailedException;
 
-	/**
-	 * Get the last version of a resource
-	 * 
-	 * @param resourceid
-	 *            Resource identifier
-	 * @return Version string
-	 */
-	public String getLastVersion(int resourceid);
+    /**
+     * Get resource categories on spigot
+     *
+     * @return List of
+     * {@link be.maximvdw.spigotsite.api.resource.ResourceCategory}
+     */
+    List<ResourceCategory> getResourceCategories();
 
-	/**
-	 * Get premium resource buyers
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated author of the plugin
-	 * @return List of {@link be.maximvdw.spigotsite.api.user.User}
-	 */
-	public List<User> getPremiumResourceBuyers(PremiumResource resource,
-			User user) throws ConnectionFailedException;
+    /**
+     * Get {@link be.maximvdw.spigotsite.api.resource.ResourceCategory} by
+     * identifier
+     *
+     * @param id Resource Category identifier
+     * @return Resource Category
+     */
+    ResourceCategory getResourceCategoryById(int id);
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param buyer
-	 *            The buyer
-	 */
-	public void addBuyer(PremiumResource resource, User user, User buyer);
+    /**
+     * Get all resources from a specific category
+     *
+     * @param category List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     * @return
+     */
+    List<Resource> getResourcesByCategory(ResourceCategory category);
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param userid
-	 *            User identifier
-	 */
-	public void addBuyer(PremiumResource resource, User user, int userid);
+    /**
+     * Get the last version of a resource
+     *
+     * @param resourceid Resource identifier
+     * @return Version string
+     */
+    String getLastVersion(int resourceid);
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param username
-	 *            The text username
-	 */
-	public void addBuyer(PremiumResource resource, User user, String username);
+    /**
+     * Get premium resource buyers
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated author of the plugin
+     * @return List of {@link be.maximvdw.spigotsite.api.user.User}
+     */
+    List<User> getPremiumResourceBuyers(PremiumResource resource,
+                                        User user) throws ConnectionFailedException;
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param buyers
-	 *            A list of buyers
-	 */
-	public void addBuyers(PremiumResource resource, User user, List<User> buyers);
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated plugin author
+     * @param buyer    The buyer
+     */
+    void addBuyer(PremiumResource resource, User user, User buyer);
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param resource
-	 *            Premium resource
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param usernames
-	 *            A list of usernames
-	 */
-	public void addBuyers(PremiumResource resource, User user,
-			String[] usernames);
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated plugin author
+     * @param userid   User identifier
+     */
+    void addBuyer(PremiumResource resource, User user, int userid);
+
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated plugin author
+     * @param username The text username
+     */
+    void addBuyer(PremiumResource resource, User user, String username);
+
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated plugin author
+     * @param buyers   A list of buyers
+     */
+    void addBuyers(PremiumResource resource, User user, List<User> buyers);
+
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param resource  Premium resource
+     * @param user      The authenticated plugin author
+     * @param usernames A list of usernames
+     */
+    void addBuyers(PremiumResource resource, User user,
+                   String[] usernames);
+
+    /**
+     * Remove a buyer from a spigot premium resource
+     *
+     * @param resource Premium resource
+     * @param user     The authenticated plugin author
+     * @param username Username to remove
+     */
+    void removeBuyer(PremiumResource resource, User user, String username);
 }
