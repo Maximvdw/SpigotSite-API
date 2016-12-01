210a426a41725f9215fc01f8e53d703737390598
Maxim Van de Wynckel <maxim.vandewynckel@gmail.com>
2016-12-01 22:44:12 +0100

    User boolean 2fa

--- src/main/java/be/maximvdw/spigotsite/api/user/User.java
+++ src/main/java/be/maximvdw/spigotsite/api/user/User.java
@@ -7,66 +7,73 @@ import be.maximvdw.spigotsite.api.resource.Resource;
 
 /**
  * Spigot User
- * 
+ *
  * @author Maxim Van de Wynckel
  */
 public interface User {
-	/**
-	 * Get spigot user identifier
-	 * 
-	 * @return Spigot user identifier
-	 */
-	int getUserId();
+    /**
+     * Get spigot user identifier
+     *
+     * @return Spigot user identifier
+     */
+    int getUserId();
 
-	/**
-	 * Get spigot username
-	 * 
-	 * @return Spigot username
-	 */
-	String getUsername();
+    /**
+     * Get spigot username
+     *
+     * @return Spigot username
+     */
+    String getUsername();
 
-	/**
-	 * Determine if the spigot user is authenticated and can be used to perform
-	 * private actions.
-	 * 
-	 * @return is Authenticated
-	 */
-	boolean isAuthenticated();
+    /**
+     * Determine if the spigot user is authenticated and can be used to perform
+     * private actions.
+     *
+     * @return is Authenticated
+     */
+    boolean isAuthenticated();
 
-	/**
-	 * Get purchased resources
-	 * 
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 * @throws ConnectionFailedException
-	 */
-	List<Resource> getPurchasedResources()
-			throws ConnectionFailedException;
+    /**
+     * Get purchased resources
+     *
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     * @throws ConnectionFailedException
+     */
+    List<Resource> getPurchasedResources()
+            throws ConnectionFailedException;
 
-	/**
-	 * Get created resources
-	 * 
-	 * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
-	 */
-	List<Resource> getCreatedResources();
+    /**
+     * Get created resources
+     *
+     * @return List of {@link be.maximvdw.spigotsite.api.resource.Resource}
+     */
+    List<Resource> getCreatedResources();
 
-	/**
-	 * Get spigot user statistics
-	 * 
-	 * @return {@link be.maximvdw.spigotsite.api.user.UserStatistics}
-	 */
-	UserStatistics getUserStatistics();
+    /**
+     * Get spigot user statistics
+     *
+     * @return {@link be.maximvdw.spigotsite.api.user.UserStatistics}
+     */
+    UserStatistics getUserStatistics();
 
-	/**
-	 * Get spigot user private conversations
-	 * 
-	 * @return List of conversation
-	 */
-	List<Conversation> getConversations();
+    /**
+     * Get spigot user private conversations
+     *
+     * @return List of conversation
+     */
+    List<Conversation> getConversations();
 
-	/**
-	 * Get users last activity
-	 * 
-	 * @return Last activity string
-	 */
-	String getLastActivity();
+    /**
+     * Get users last activity
+     *
+     * @return Last activity string
+     */
+    String getLastActivity();
+
+    /**
+     * Check if the user has two factory authentication
+     *
+     * @return two factor auth
+     */
+    boolean hasTwoFactorAuthentication();
 }
