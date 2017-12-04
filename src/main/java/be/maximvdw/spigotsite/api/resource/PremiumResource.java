b4c30fd152bf8a0777bfa29a415395f5ccf5e46c
Maximvdw <maxim@mvdw-software.com>
2017-12-05 00:54:45 +0100

    0.2.0

--- src/main/java/be/maximvdw/spigotsite/api/resource/PremiumResource.java
+++ src/main/java/be/maximvdw/spigotsite/api/resource/PremiumResource.java
@@ -1,100 +1,97 @@
 package be.maximvdw.spigotsite.api.resource;
 
 import java.util.List;
+import java.util.Set;
 
 import be.maximvdw.spigotsite.api.exceptions.ConnectionFailedException;
 import be.maximvdw.spigotsite.api.user.User;
 
 /**
  * Spigot Premium Resource
- * 
+ *
  * @author Maxim Van de Wynckel
  */
 public interface PremiumResource extends Resource {
-	/**
-	 * Get resource price
-	 * 
-	 * @return Price
-	 */
+    /**
+     * Get resource price
+     *
+     * @return Price
+     */
     double getPrice();
 
-	/**
-	 * Set resource price
-	 * 
-	 * @param price
-	 *            Price
-	 */
+    /**
+     * Set resource price
+     *
+     * @param price Price
+     */
     void setPrice(double price);
 
-	/**
-	 * Get resource price currency
-	 * 
-	 * @return Price currency
-	 */
+    /**
+     * Get resource price currency
+     *
+     * @return Price currency
+     */
     String getPriceCurrency();
 
-	/**
-	 * Set resource price currency
-	 * 
-	 * @param currency
-	 *            Price currency
-	 */
+    /**
+     * Set resource price currency
+     *
+     * @param currency Price currency
+     */
     void setPriceCurrency(String currency);
 
-	/**
-	 * Get premium resource buyers
-	 * 
-	 * @return Resource buyers
-	 */
-    List<Buyer> getBuyers();
+    /**
+     * Get premium resource buyers
+     *
+     * @return Resource buyers
+     */
+    Set<Buyer> getBuyers();
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param buyer
-	 *            The buyer
-	 */
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param user  The authenticated plugin author
+     * @param buyer The buyer
+     */
     void addBuyer(User user, User buyer) throws ConnectionFailedException;
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param userid
-	 *            User identifier
-	 */
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param user   The authenticated plugin author
+     * @param userid User identifier
+     */
     void addBuyer(User user, int userid) throws ConnectionFailedException;
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param username
-	 *            The text username
-	 */
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param user     The authenticated plugin author
+     * @param username The text username
+     */
     void addBuyer(User user, String username) throws ConnectionFailedException;
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param buyers
-	 *            A list of buyers
-	 */
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param user   The authenticated plugin author
+     * @param buyers A list of buyers
+     */
     void addBuyers(User user, List<User> buyers) throws ConnectionFailedException;
 
-	/**
-	 * Add a buyer to a spigot premium resource
-	 * 
-	 * @param user
-	 *            The authenticated plugin author
-	 * @param usernames
-	 *            A list of usernames
-	 */
+    /**
+     * Add a buyer to a spigot premium resource
+     *
+     * @param user      The authenticated plugin author
+     * @param usernames A list of usernames
+     */
     void addBuyers(User user, String[] usernames) throws ConnectionFailedException;
+
+    /**
+     * Check if a specific user is a buyer
+     *
+     * @param user User is buyer
+     * @return is buyer
+     */
+    boolean isBuyer(User user);
 }
